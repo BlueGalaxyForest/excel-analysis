4347ecf4b954a1f414af231c3508a538c567a5dd
何懋林 <2211634606@qq.com>
2023-10-23 22:04:08 +0800

    home_init_project

---
+++ excelAnalysis_front/excel-analysis-front/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app">
+    <div class="left-side">
+      哈哈
+    </div>
+    <div class="right-side">
+
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .app {
+    .left-side{
+      color:red
+    }
+  
+  }
+
+    
+  
+
+</style>
