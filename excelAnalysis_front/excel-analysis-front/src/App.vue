6f0c7d7f4459a8c8ac3cf2a51639ca281b1db503
何懋林 <2211634606@qq.com>
2023-10-24 22:16:20 +0800

    home_引入elementuiplus

--- excelAnalysis_front/excel-analysis-front/src/App.vue
+++ excelAnalysis_front/excel-analysis-front/src/App.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElButton } from 'element-plus'
+ 
+
+</script>
 
 <template>
   <div class="app">
@@ -6,6 +10,7 @@
       哈哈
     </div>
     <div class="right-side">
+      <el-button>Click me</el-button>
 
     </div>
   </div>
